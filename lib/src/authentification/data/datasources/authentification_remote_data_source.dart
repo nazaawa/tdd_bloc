34ae610b00cb029d12e2b7ab932736f201325345
Johnny Andedi <johnny.yetsha@schoolap.org>
2023-12-04 14:59:27 +0100

    refactor: Simplify createUser method signature
    This commit simplifies the method signature of the `createUser` method in `AuthentificationRemoteDataSource` by removing unnecessary code formatting and aligning the parameter list. This change enhances code readability and maintains consistency within the codebase.
    
    No issue references.

--- lib/src/authentification/data/datasources/authentification_remote_data_source.dart
+++ lib/src/authentification/data/datasources/authentification_remote_data_source.dart
@@ -1,11 +1,10 @@
 import 'package:tdd_tutorial/src/authentification/data/models/user_model.dart';
 
 abstract class AuthentificationRemoteDataSource {
-  Future<void> createUser({
-     required String name,
-    required String avatar,
-    required String createdAt,
-  });
+  Future<void> createUser(
+      {required String name,
+      required String avatar,
+      required String createdAt});
 
   Future<List<UserModel>> getUsers();
 }
